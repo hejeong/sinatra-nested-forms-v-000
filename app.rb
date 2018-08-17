cbeac7367ee703255e0eccb0ba590f3c4b6bcaf5
Jonathan Hong <jonhong777@gmail.com>
2018-08-17 05:06:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module FormsLab
 
     post '/pirates' do
       @pirates = Pirate.new(params["pirate"])
-      
+      params["pirate"]
     #  @pirates = Pirate.new(params)
   #    @ship = Ship.new(params)
       erb :'pirates/show'
